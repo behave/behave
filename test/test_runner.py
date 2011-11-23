da8c767ef758ce456e331a3f42475ef9b98a9f4f
Benno Rice <benno@jeamland.net>
2011-11-23 22:28:13 +1100

    Add tests for StepRegistry.

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,3 +1,4 @@
+from mock import Mock, patch
 from nose.tools import *
 
 from behave import runner
@@ -5,18 +6,18 @@ from behave import runner
 class TestContext(object):
     def setUp(self):
         self.context = runner.Context()
-        
+
     def test_attribute_set_at_upper_level_visible_at_lower_level(self):
         self.context.thing = 'stuff'
         self.context._push()
         eq_(self.context.thing, 'stuff')
-    
+
     def test_attribute_set_at_lower_level_not_visible_at_upper_level(self):
         self.context._push()
         self.context.thing = 'stuff'
         self.context._pop()
         assert getattr(self.context, 'thing', None) is None
-    
+
     def test_attributes_set_at_upper_level_visible_at_lower_level(self):
         self.context.thing = 'stuff'
         self.context._push()
@@ -48,3 +49,75 @@ class TestContext(object):
         eq_(self.context.thing, 'stuff')
         assert getattr(self.context, 'other_thing', None) is None
         assert getattr(self.context, 'third_thing', None) is None
+
+class TestStepRegistry(object):
+    def test_add_definition_adds_to_lowercased_keyword(self):
+        registry = runner.StepRegistry()
+        with patch('behave.matchers.get_matcher') as get_matcher:
+            func = lambda x: -x
+            string = 'just a test string'
+            magic_object = object()
+            get_matcher.return_value = magic_object
+
+            for step_type in registry.steps.keys():
+                mock = Mock()
+                registry.steps[step_type] = mock
+
+                registry.add_definition(step_type.upper(), string, func)
+
+                get_matcher.assert_called_with(func, string)
+                mock.append.assert_called_with(magic_object)
+
+    def test_find_match_with_specific_step_type_also_searches_generic(self):
+        registry = runner.StepRegistry()
+
+        given_mock = Mock()
+        given_mock.match.return_value = None
+        step_mock = Mock()
+        step_mock.match.return_value = None
+
+        registry.steps['given'].append(given_mock)
+        registry.steps['step'].append(step_mock)
+
+        step = Mock()
+        step.step_type = 'given'
+        step.name = 'just a test step'
+
+        assert registry.find_match(step) is None
+
+        given_mock.match.assert_called_with(step.name)
+        step_mock.match.assert_called_with(step.name)
+
+    def test_find_match_with_no_match_returns_none(self):
+        registry = runner.StepRegistry()
+
+        step_defs = [Mock() for x in range(0, 10)]
+        for mock in step_defs:
+            mock.match.return_value = None
+
+        registry.steps['when'] = step_defs
+
+        step = Mock()
+        step.step_type = 'when'
+        step.name = 'just a test step'
+
+        assert registry.find_match(step) is None
+
+    def test_find_match_with_a_match_returns_match(self):
+        registry = runner.StepRegistry()
+
+        step_defs = [Mock() for x in range(0, 10)]
+        for mock in step_defs:
+            mock.match.return_value = None
+        magic_object = object()
+        step_defs[5].match.return_value = magic_object
+
+        registry.steps['then'] = step_defs
+
+        step = Mock()
+        step.step_type = 'then'
+        step.name = 'just a test step'
+
+        assert registry.find_match(step) is magic_object
+        for mock in step_defs[6:]:
+            eq_(mock.match.call_count, 0)
