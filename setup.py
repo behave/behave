983bbb1ee0ec8f83607b060caaeeb22ed2bd9dde
Richard Jones <r1chardj0n3s@gmail.com>
2011-11-25 16:32:53 +1100

    declare py3k support; fixes #13

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         "Topic :: Software Development :: Testing",
         "License :: OSI Approved :: BSD License",
     ],
