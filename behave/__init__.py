11cba961e63887aff59aaea23cf17aad5016fcad
Richard Jones <r1chardj0n3s@gmail.com>
2011-12-08 15:37:36 +1100

    bump version number up

--- behave/__init__.py
+++ behave/__init__.py
@@ -15,7 +15,7 @@ To get started, we recommend the `tutorial`_ and then the `test language`_ and
 .. _`api`: api.html
 '''
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from behave.step_registry import *
 from behave.matchers import step_matcher
