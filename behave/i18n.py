5d6f8235c250cc80ec6f447ee15ca5cc04b49d10
jenisys <jens_engel@web.de>
2012-02-21 23:43:38 +0100

    language=de: Correct Background=>Hintergrund (was: Grundgeruest).

--- behave/i18n.py
+++ behave/i18n.py
@@ -79,7 +79,7 @@ languages = \
         'then': [u'*', u'S\xe5'],
         'when': [u'*', u'N\xe5r']},
  'de': {'and': [u'*', u'Und'],
-        'background': [u'Grundlage'],
+        'background': [u'Hintergrund'],
         'but': [u'*', u'Aber'],
         'examples': [u'Beispiele'],
         'feature': [u'Funktionalit\xe4t'],
