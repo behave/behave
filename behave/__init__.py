20614141c4e4f978aa936bcbbb75fe230aed2f06
jenisys <jens_engel@web.de>
2012-04-11 18:55:07 +0200

    BUMP-VERSION: 1.1.0.4

--- behave/__init__.py
+++ behave/__init__.py
@@ -18,7 +18,7 @@ To get started, we recommend the `tutorial`_ and then the `test language`_ and
 .. _`api`: api.html
 '''
 
-__version__ = '1.1.0.3'
+__version__ = '1.1.0.4'
 
 from behave.step_registry import *
 from behave.matchers import step_matcher
