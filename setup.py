d5302c808a7fee5810603505ed2490eafa66cf9e
Benno Rice <benno@jeamland.net>
2012-05-22 11:34:04 +1000

    Attempt to get around the distribute glitch we're seeing in 3.3.

--- setup.py
+++ setup.py
@@ -5,11 +5,16 @@ import sys
 from setuptools import find_packages, setup
 
 requirements = ['parse>=1.3.3']
+zip_safe = True
 major, minor = sys.version_info[:2]
 if major == 2 and minor < 7:
     requirements.append('argparse')
 if major == 2 and minor < 6:
     requirements.append('simplejson')
+if major == 3 and minor >= 3:
+    # distribute appears to much with co_names in a way that Python 3.3
+    # doesn't appreciate.
+    zip_safe = False
 
 description = ''.join(open('README.rst').readlines()[6:])
 
@@ -25,6 +30,7 @@ setup(
     scripts=['bin/behave'],
     install_requires=requirements,
     use_2to3=True,
+    zip_safe=zip_safe,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
@@ -34,6 +40,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: Jython",
         "Programming Language :: Python :: Implementation :: PyPy",
