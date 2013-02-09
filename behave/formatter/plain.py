a7494e848190873785506ea1d2fcbf19dd5b7203
jenisys <jens_engel@web.de>
2013-02-09 15:42:17 +0100

    FIX issue #35:  --format=plain w/ tags filtering showed wrong steps. Reset steps now.

--- behave/formatter/plain.py
+++ behave/formatter/plain.py
@@ -1,26 +1,37 @@
+# -*- coding: utf-8 -*-
+
 from behave.formatter.base import Formatter
 
 
 class PlainFormatter(Formatter):
+    """
+    Provides a simple plain formatter without coloring/formatting.
+    In addition, multi-line text and tables are not shown in output (SAD).
+    """
     name = 'plain'
     description = 'Very basic formatter with maximum compatibility'
 
     def __init__(self, stream, config):
         super(PlainFormatter, self).__init__(stream, config)
         self.steps = []
-
         self.show_timings = config.show_timings
 
+    def reset_steps(self):
+        self.steps = []
+
     def feature(self, feature):
+        self.reset_steps()
         self.stream.write(u'%s: %s\n' % (feature.keyword, feature.name))
 
     def background(self, background):
         self.stream.write(u'%s: %s\n' % (background.keyword, background.name))
 
     def scenario(self, scenario):
+        self.reset_steps()
         self.stream.write(u'%11s: %s\n' % (scenario.keyword, scenario.name))
 
     def scenario_outline(self, outline):
+        self.reset_steps()
         self.stream.write(u' %s: %s\n' % (outline.keyword, outline.name))
 
     def step(self, step):
