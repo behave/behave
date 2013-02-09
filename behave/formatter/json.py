bc3005904730aa0b2811653de9166dae7dbda472
jenisys <jens_engel@web.de>
2013-02-09 18:25:10 +0100

    FIX issue #67: JSON formatter cannot serialize step tables.

--- behave/formatter/json.py
+++ behave/formatter/json.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
 import base64
@@ -40,6 +41,10 @@ class JSONFormatter(Formatter):
         })
         self._step_index = 0
 
+        # -- ADD BACKGROUND STEPS: Support *.feature file regeneration.
+        for step_ in background.steps:
+            self.step(step_)
+
     def scenario(self, scenario):
         self._add_feature_element({
             'keyword': scenario.keyword,
@@ -61,6 +66,14 @@ class JSONFormatter(Formatter):
         })
         self._step_index = 0
 
+    @classmethod
+    def make_table(cls, table):
+        table_data = {
+            'headings': table.headings,
+            'rows': [ list(row) for row in table.rows ]
+        }
+        return table_data
+
     def examples(self, examples):
         e = {
             'keyword': examples.keyword,
@@ -69,10 +82,7 @@ class JSONFormatter(Formatter):
         }
 
         if examples.table:
-            e['table'] = {
-                'headings': examples.table.headings,
-                'rows': examples.table.rows,
-            }
+            e['table'] = self.make_table(examples.table)
 
         element = self._feature_element()
         element['examples'].append(e)
@@ -88,11 +98,7 @@ class JSONFormatter(Formatter):
         if step.text:
             s['text'] = step.text
         if step.table:
-            s['table'] = {
-                'headings': step.table.headings,
-                'rows': step.table.rows,
-            }
-
+            s['table'] = self.make_table(step.table)
         element = self._feature_element()
         element['steps'].append(s)
 
@@ -151,4 +157,4 @@ class JSONFormatter(Formatter):
 class PrettyJSONFormatter(JSONFormatter):
     name = 'json-pretty'
     description = 'JSON dump of test run (human readable)'
-    dumps_kwargs = {'indent': 2}
+    dumps_kwargs = { 'indent': 2, 'sort_keys': True }
