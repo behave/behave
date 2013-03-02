ad2dcc8bcce6a5e0c0d3294fed0ce5d85e681a14
jenisys <jens_engel@web.de>
2013-03-02 15:48:28 +0100

    Tweak/correct name

--- features/environment.py
+++ features/environment.py
@@ -4,11 +4,11 @@
 # HOOKS:
 # -----------------------------------------------------------------------------
 def before_all(context):
-    setup_context_with_environment_test(context)
+    setup_context_with_global_params_test(context)
 
 # -----------------------------------------------------------------------------
 # SPECIFIC FUNCTIONALITY:
 # -----------------------------------------------------------------------------
-def setup_context_with_environment_test(context):
+def setup_context_with_global_params_test(context):
     context.global_name = "env:Alice"
     context.global_age  = 12
