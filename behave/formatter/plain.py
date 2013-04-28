cb89534b66df5c69f2cbf6583992fe7545bad291
jenisys <jens_engel@web.de>
2013-04-28 15:47:46 +0200

    Plain formatter prints now multi-line step parts (text/doc-string, table). Refactor out common parts to behave.model_describe (and reuse them). Adapt pretty formatter accordingly.

--- behave/formatter/plain.py
+++ behave/formatter/plain.py
@@ -1,52 +1,127 @@
 # -*- coding: utf-8 -*-
 
 from behave.formatter.base import Formatter
+from behave.model_describe import ModelPrinter, make_indentation
 
 
+# -----------------------------------------------------------------------------
+# CLASS: PlainFormatter
+# -----------------------------------------------------------------------------
 class PlainFormatter(Formatter):
     """
     Provides a simple plain formatter without coloring/formatting.
-    In addition, multi-line text and tables are not shown in output (SAD).
+    The formatter displays now also:
+
+       * multi-line text (doc-strings)
+       * table
     """
     name = 'plain'
     description = 'Very basic formatter with maximum compatibility'
 
-    def __init__(self, stream, config):
+    SHOW_ALIGNED_KEYWORDS = False
+    DEFAULT_INDENT_SIZE = 2
+    ENABLE_MULTI_LINE = True
+
+    def __init__(self, stream, config, **kwargs):
         super(PlainFormatter, self).__init__(stream, config)
         self.steps = []
         self.show_timings = config.show_timings
+        self.show_multiline = config.show_multiline and self.ENABLE_MULTI_LINE
+        self.show_aligned_keywords = self.SHOW_ALIGNED_KEYWORDS
+        self.indent_size = self.DEFAULT_INDENT_SIZE
+        self.printer = ModelPrinter(stream)
+        # -- LAZY-EVALUATE:
+        self._multiline_indentation = None
+
+    @property
+    def multiline_indentation(self):
+        if self._multiline_indentation is None:
+            offset = 0
+            if self.show_aligned_keywords:
+                offset = 2
+            indentation = make_indentation(3 * self.indent_size + offset)
+            self._multiline_indentation = indentation
+        return self._multiline_indentation
 
     def reset_steps(self):
         self.steps = []
 
+    # -- IMPLEMENT-INTERFACE FOR: Formatter
     def feature(self, feature):
         self.reset_steps()
         self.stream.write(u'%s: %s\n' % (feature.keyword, feature.name))
 
     def background(self, background):
-        self.stream.write(u'%s: %s\n' % (background.keyword, background.name))
+        self.reset_steps()
+        indent = make_indentation(self.indent_size)
+        text = u'%s%s: %s\n' % (indent, background.keyword, background.name)
+        self.stream.write(text)
 
     def scenario(self, scenario):
         self.reset_steps()
-        self.stream.write(u'%11s: %s\n' % (scenario.keyword, scenario.name))
+        self.stream.write(u'\n')
+        indent = make_indentation(self.indent_size)
+        text = u'%s%s: %s\n' % (indent, scenario.keyword, scenario.name)
+        self.stream.write(text)
 
     def scenario_outline(self, outline):
         self.reset_steps()
-        self.stream.write(u' %s: %s\n' % (outline.keyword, outline.name))
+        indent = make_indentation(self.indent_size)
+        text = u'%s%s: %s\n' % (indent, outline.keyword, outline.name)
+        self.stream.write(text)
 
     def step(self, step):
         self.steps.append(step)
 
     def result(self, result):
+        """
+        Process the result of a step (after step execution).
+
+        :param result:
+        """
         step = self.steps.pop(0)
-        # TODO right-align the keyword to maximum keyword width?
-        self.stream.write(u'%12s %s ... ' % (step.keyword, step.name))
+        indent = make_indentation(2 * self.indent_size)
+        if self.show_aligned_keywords:
+            # -- RIGHT-ALIGN KEYWORDS (max. keyword width: 6):
+            text = u'%s%6s %s ... ' % (indent, step.keyword, step.name)
+        else:
+            text = u'%s%s %s ... ' % (indent, step.keyword, step.name)
+        self.stream.write(text)
 
         status = result.status
         if self.show_timings:
-            status += " in %0.2fs" % step.duration
+            status += " in %0.3fs" % step.duration
 
         if result.error_message:
             self.stream.write(u'%s\n%s\n' % (status, result.error_message))
         else:
             self.stream.write(u'%s\n' % status)
+
+        if self.show_multiline:
+            if step.text:
+                self.doc_string(step.text)
+            if step.table:
+                self.table(step.table)
+
+    # -- MORE: Formatter helpers
+    def doc_string(self, doc_string):
+        self.printer.print_docstring(doc_string, self.multiline_indentation)
+
+    def table(self, table):
+        self.printer.print_table(table, self.multiline_indentation)
+
+
+# -----------------------------------------------------------------------------
+# CLASS: Plain0Formatter
+# -----------------------------------------------------------------------------
+class Plain0Formatter(PlainFormatter):
+    """
+    Similar to old plain formatter without support for:
+
+      * multi-line text
+      * tables
+    """
+    name = 'plain0'
+    description = 'Very basic formatter with maximum compatibility'
+    ENABLE_MULTI_LINE = False
+
---
+++ behave/model_describe.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Provides textual descriptions for :mod:`behave.model` elements.
+"""
+
+# -----------------------------------------------------------------------------
+# FUNCTIONS:
+# -----------------------------------------------------------------------------
+def make_indentation(indent_size, part=u" "):
+    """
+    Creates an indentation prefix string of the given size.
+    """
+    return indent_size * part
+
+
+def indent(text, prefix):
+    """
+    Indent text or a number of text lines (with newline).
+
+    :param lines:  Text lines to indent (as string or list of strings).
+    :param prefix: Line prefix to use (as string).
+    :return: Indented text (as unicode string).
+    """
+    lines = text
+    newline = u""
+    if isinstance(text, basestring):
+        lines = text.splitlines(True)
+    elif lines and not lines[0].endswith("\n"):
+        # -- TEXT LINES: Without trailing new-line.
+        newline = u"\n"
+    return newline.join([prefix + unicode(line) for line in lines])
+
+
+def escape_cell(cell):
+    """
+    Escape table cell contents.
+    :param cell:  Table cell (as unicode string).
+    :return: Escaped cell (as unicode string).
+    """
+    cell = cell.replace(u'\\', u'\\\\')
+    cell = cell.replace(u'\n', u'\\n')
+    cell = cell.replace(u'|', u'\\|')
+    return cell
+
+
+def escape_triple_quotes(text):
+    """
+    Escape triple-quotes, used for multi-line text/doc-strings.
+    """
+    return text.replace(u'"""', u'\\"\\"\\"')
+
+
+# -----------------------------------------------------------------------------
+# CLASS:
+# -----------------------------------------------------------------------------
+class ModelDescriptor(object):
+
+    @staticmethod
+    def describe_table(table, indentation=None):
+        """
+        Provide a textual description of the table (as used w/ Gherkin).
+
+        :param table:  Table to use (as :class:`behave.model.Table`)
+        :param indentation:  Line prefix to use (as string, if any).
+        :return: Textual table description (as unicode string).
+        """
+        # -- STEP: Determine output size of all cells.
+        cell_lengths = []
+        all_rows = [table.headings] + table.rows
+        for row in all_rows:
+            lengths = [len(escape_cell(c)) for c in row]
+            cell_lengths.append(lengths)
+
+        # -- STEP: Determine max. output size for each column.
+        max_lengths = []
+        for col in range(0, len(cell_lengths[0])):
+            max_lengths.append(max([c[col] for c in cell_lengths]))
+
+        # -- STEP: Build textual table description.
+        lines = []
+        for r, row in enumerate(all_rows):
+            line = u"|"
+            for c, (cell, max_length) in enumerate(zip(row, max_lengths)):
+                pad_size = max_length - cell_lengths[r][c]
+                line += u" %s%s |" % (escape_cell(cell), " " * pad_size)
+            line += u"\n"
+            lines.append(line)
+
+        if indentation:
+            return indent(lines, indentation)
+        # -- OTHERWISE:
+        text = u"".join(lines)
+        return text
+
+
+    @staticmethod
+    def describe_docstring(doc_string, indentation=None):
+        """
+        Provide a textual description of the multi-line text/triple-quoted
+        doc-string (as used w/ Gherkin).
+
+        :param doc_string:  Multi-line text to use.
+        :param indentation:  Line prefix to use (as string, if any).
+        :return: Textual table description (as unicode string).
+        """
+        text = escape_triple_quotes(doc_string)
+        text = u'"""\n'+ text +'\n"""\n'
+
+        if indentation:
+            text = indent(text, indentation)
+        return text
+
+
+class ModelPrinter(ModelDescriptor):
+
+    def __init__(self, stream):
+        super(ModelPrinter, self).__init__()
+        self.stream = stream
+
+    def print_table(self, table, indentation=None):
+        self.stream.write(self.describe_table(table, indentation))
+        self.stream.flush()
+
+    def print_docstring(self, text, indentation=None):
+        self.stream.write(self.describe_docstring(text, indentation))
+        self.stream.flush()
