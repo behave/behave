ee2942aff78abe323662337db8ce76cddcf8a4b6
jenisys <jens_engel@web.de>
2013-07-08 22:46:33 +0200

    BUMP-VERSION: 1.2.3 (was: 1.2.3a20) and release it.

--- behave/__init__.py
+++ behave/__init__.py
@@ -16,7 +16,7 @@ To get started, we recommend the `tutorial`_ and then the `test language`_ and
 .. _`api`: api.html
 '''
 
-__version__ = '1.2.3a20'
+__version__ = '1.2.3'
 
 from behave.step_registry import *
 from behave.matchers import step_matcher
