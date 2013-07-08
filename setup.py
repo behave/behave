5ccc19f8487882180582b553778d5922a564f9a2
jenisys <jens_engel@web.de>
2013-07-08 22:46:33 +0200

    BUMP-VERSION: 1.2.3 (was: 1.2.3a20) and release it.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ description = ''.join(open('README.rst').readlines()[5:])
 
 setup(
     name='behave',
-    version='1.2.3a20',
+    version='1.2.3',
     description='behave is behaviour-driven development, Python style',
     long_description=description,
     author='Benno Rice, Richard Jones and Jens Engel',
