a196c249a2b48cddf10398c29cfe518fc71277e0
jenisys <jenisys@users.noreply.github.com>
2014-05-26 22:08:09 +0200

    Various improvements and refactorings: - removed lxml dependency (and added pretty print). - Replaced Javascript snippet copies w/ Javascript calls. - Moved CSS stylesheet out to make it easier to replace.

--- behave/formatter/html.py
+++ behave/formatter/html.py
@@ -1,8 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+HTML formatter for behave.
+Writes a single-page HTML file for test run with all features/scenarios.
+
+
+IMPROVEMENTS:
+  + Avoid to use lxml.etree, use xml.etree.ElementTree instead (bundled w/ Python)
+  + Add pretty_print functionality to provide lxml goodie.
+  + Stylesheet should be (easily) replacable
+  + Simplify collapsable-section usage:
+    => Only JavaScript-call: onclick = Collapsible_toggle('xxx')
+    => Avoid code duplications, make HTML more readable
+  + Expand All / Collapse All: Use <a> instead of <span> element
+    => Make active logic (actions) more visible
+  * Support external stylesheet ?!?
+  * Introduce (Html)Page class to simplify extension and replacements
+  * Separate business layer (HtmlFormatter) from technology layer (Page).
+  * Correct Python2 constructs: map()/reduce()
+  * end() or stream.close() handling is missing
+  * steps: text, table parts are no so easily detectable
+  * CSS: stylesheet should contain logical "style" classes.
+    => AVOID using combination of style attributes where style is better.
+
+TODO:
+  * After pretty-printing: HTML has XML PI
+        <?xml version="1.0" ?>
+        <html>
+  * Embedding only works with one part ?!?
+  * Even empty embed elements are contained ?!?
+
+IDEA:
+  * "Expand Failed" or "Show Failed" action (on collapsible scenarios).
+"""
+
 from behave.formatter.base import Formatter
-import lxml.etree as ET
-import base64
-import os.path
 from behave.compat.collections import Counter
+# XXX-JE-OLD: import lxml.etree as ET
+import xml.etree.ElementTree as ET
+import base64
+# XXX-JE-NOT-USED: import os.path
 
 
 def _valid_XML_char_ordinal(i):
@@ -11,79 +47,155 @@ def _valid_XML_char_ordinal(i):
         or i in (0x9, 0xA, 0xD)
         or 0xE000 <= i <= 0xFFFD
         or 0x10000 <= i <= 0x10FFFF
-        )
+    )
+
+# XXX-JE-FIRST-IDEA:
+# def html_prettify(elem):
+#     """Return a pretty-printed XML string for the Element."""
+#     rough_string = ET.tostring(elem, "utf-8") # XXX, method="html")
+#     reparsed = minidom.parseString(rough_string)
+#     return reparsed.toprettyxml(indent="  ")
+
+def ET_tostring(elem, pretty_print=False):
+    """Render an HTML element(tree) and optionally pretty-print it."""
+    text = ET.tostring(elem, "utf-8")   # XXX, method="html")
+    if pretty_print:
+        # -- RECIPE: For pretty-printing w/ xml.etree.ElementTree.
+        # SEE: http://pymotw.com/2/xml/etree/ElementTree/create.html
+        from xml.dom import minidom
+        reparsed = minidom.parseString(text)
+        text = reparsed.toprettyxml(indent="  ")
+    return text
+
+class JavascriptLibrary(object):
+    collapsible = """
+function Collapsible_toggle(id)
+{
+    var elem = document.getElementById(id);
+    elem.style.display = (elem.style.display == 'none' ? 'block' : 'none');
+    return false;
+}
+
+function Collapsible_expandAll(className)
+{
+    var elems = document.getElementsByClassName(className);
+    for (var i=0; i < elems.length; i++) {
+        elems[i].style.display = 'block';
+    }
+}
+
+function Collapsible_collapseAll(className)
+{
+    var elems = document.getElementsByClassName(className);
+    for (var i=0; i < elems.length; i++) {
+        elems[i].style.display = 'none';
+    }
+}
+"""
+
+
+class BasicTheme(object):
+    stylesheet_text = """
+body{font-size:0;color:#fff;margin:0;
+padding:0}.behave,td,th{font:400 11px "Lucida Grande",Helvetica,sans-serif;
+background:#fff;color:#000}.behave #behave-header,td #behave-header,
+th #behave-header{background:#65c400;color:#fff;height:8em}.behave
+#behave-header #expand-collapse p,td #behave-header #expand-collapse
+p,th #behave-header #expand-collapse p{float:right;margin:0 0 0 10px}
+.background h3,.behave .scenario h3,td .scenario h3,th .scenario h3{
+font-size:11px;padding:3px;margin:0;background:#65c400;color:#fff;
+font-weight:700}.background h3{font-size:1.2em;background:#666}.behave
+h1,td h1,th h1{margin:0 10px;padding:10px;font-family:'Lucida Grande',
+Helvetica,sans-serif;font-size:2em;position:absolute}.behave h4,td h4,
+th h4{margin-bottom:2px}.behave div.feature,td div.feature,th div.feature
+{padding:2px;margin:0 10px 5px}.behave div.examples,td div.examples,th
+div.examples{padding:0 0 0 1em}.behave .stats,td .stats,th .stats{margin:2em}
+.behave .summary ul.features li,td .summary ul.features li,th .summary
+ul.features li{display:inline}.behave .step_name,td .step_name,th .step_name
+{float:left}.behave .step_file,td .step_file,th .step_file{text-align:right;
+color:#999}.behave .step_file a,td .step_file a,th .step_file a{color:#999}.behave
+.scenario_file,td .scenario_file,th .scenario_file{float:right;color:#999}.behave
+.tag,td .tag,th .tag{font-weight:700;color:#246ac1}.behave .backtrace,td
+.backtrace,th .backtrace{margin-top:0;margin-bottom:0;margin-left:1em;color:#000}
+.behave a,td a,th a{text-decoration:none;color:#be5c00}.behave a:hover,
+td a:hover,th a:hover{text-decoration:underline}.behave a:visited,td a:visited,
+th a:visited{font-weight:400}.behave a div.examples,td a div.examples,
+th a div.examples{margin:5px 0 5px 15px;color:#000}.behave .outline table,
+td .outline table,th .outline table{margin:0 0 5px 10px}.behave table,
+td table,th table{border-collapse:collapse}.behave table td,td table td,
+th table td{padding:3px 3px 3px 5px}.behave table td.failed,td table td.failed,
+th table td.failed{border-left:5px solid #c20000;border-bottom:1px solid
+#c20000;background:#fffbd3;color:#c20000}.behave table td.passed,td table
+td.passed,th table td.passed{border-left:5px solid #65c400;border-bottom:1px
+solid #65c400;background:#dbffb4;color:#3d7700}.behave table td.skipped,td
+table td.skipped,th table td.skipped{border-left:5px solid #0ff;border-bottom:1px
+solid #0ff;background:#e0ffff;color:#011}.behave table td.pending,.behave table
+td.undefined,td table td.pending,td table td.undefined,th table td.pending,th table
+td.undefined{border-left:5px solid #faf834;border-bottom:1px solid #faf834;
+background:#fcfb98;color:#131313}.behave table td.message,td table td.message,th
+table td.message{border-left:5px solid #0ff;border-bottom:1px solid #0ff;
+background:#e0ffff;color:#011}.behave ol,td ol,th ol{list-style:none;
+margin:0;padding:0}.behave ol li.step,td ol li.step,th ol li.step{
+padding:3px 3px 3px 18px;margin:5px 0 5px 5px}.behave ol li,td ol li,th
+ol li{margin:0 0 0 1em;padding:0 0 0 .2em}.behave ol li span.param,td
+ol li span.param,th ol li span.param{font-weight:700}.behave ol li.failed,td
+ol li.failed,th ol li.failed{border-left:5px solid #c20000;border-bottom:1px
+solid #c20000;background:#fffbd3;color:#c20000}.behave ol li.passed,td ol
+li.passed,th ol li.passed{border-left:5px solid #65c400;border-bottom:1px
+solid #65c400;background:#dbffb4;color:#3d7700}.behave ol li.skipped,td ol
+li.skipped,th ol li.skipped{border-left:5px solid #0ff;border-bottom:1px
+solid #0ff;background:#e0ffff;color:#011}.behave ol li.pending,.behave ol
+li.undefined,td ol li.pending,td ol li.undefined,th ol li.pending,th ol
+li.undefined{border-left:5px solid #faf834;border-bottom:1px solid
+#faf834;background:#fcfb98;color:#131313}.behave ol li.message,td ol
+li.message,th ol li.message{border-left:5px solid #0ff;border-bottom:1px
+solid #0ff;background:#e0ffff;color:#011;margin-left:10px}.behave #summary,td
+#summary,th #summary{margin:0;padding:5px 10px;text-align:right;top:0;
+right:0;float:right}.behave #summary p,td #summary p,th #summary
+p{margin:0 0 0 2px}.behave #summary #totals,td #summary #totals,th
+#summary #totals{font-size:1.2em}
+"""
+
+
+class Page(object):
+    """
+    Provides a HTML page construct (as technological layer).
+    XXX
+    """
+    theme = BasicTheme
+
+    def __init__(self, title=None):
+        pass
 
 
 class HTMLFormatter(Formatter):
+    """Provides a single-page HTML formatter
+    that writes the result of a  test run.
+    """
     name = 'html'
     description = 'Very basic HTML formatter'
+    title = u"Behave Test Report"
 
     def __init__(self, stream, config):
         super(HTMLFormatter, self).__init__(stream, config)
 
+        # -- XXX-JE-PREPARED-BUT-DISABLED:
+        # XXX Seldom changed value.
+        # XXX Should only be in configuration-file in own section "behave.formatter.html" ?!?
+        # XXX Config support must be provided.
+        # XXX REASON: Don't clutter behave config-space w/ formatter/plugin related config data.
+        # self.css = self.default_css
+        # if config.css is not None:
+        #    self.css = config.css
         self.html = ET.Element('html')
-
         head = ET.SubElement(self.html, 'head')
-        ET.SubElement(head, 'title').text = u'Behave steps'
+        ET.SubElement(head, 'title').text = self.title
         ET.SubElement(head, 'meta', {'content': 'text/html;charset=utf-8'})
-        ET.SubElement(head, 'style').text = """body{font-size:0;color:#fff;margin:0;
-        padding:0}.behave,td,th{font:400 11px "Lucida Grande",Helvetica,sans-serif;
-        background:#fff;color:#000}.behave #behave-header,td #behave-header,
-        th #behave-header{background:#65c400;color:#fff;height:8em}.behave 
-        #behave-header #expand-collapse p,td #behave-header #expand-collapse 
-        p,th #behave-header #expand-collapse p{float:right;margin:0 0 0 10px}
-        .background h3,.behave .scenario h3,td .scenario h3,th .scenario h3{
-        font-size:11px;padding:3px;margin:0;background:#65c400;color:#fff;
-        font-weight:700}.background h3{font-size:1.2em;background:#666}.behave 
-        h1,td h1,th h1{margin:0 10px;padding:10px;font-family:"Lucida Grande",
-        Helvetica,sans-serif;font-size:2em;position:absolute}.behave h4,td h4,
-        th h4{margin-bottom:2px}.behave div.feature,td div.feature,th div.feature
-        {padding:2px;margin:0 10px 5px}.behave div.examples,td div.examples,th 
-        div.examples{padding:0 0 0 1em}.behave .stats,td .stats,th .stats{margin:2em}
-        .behave .summary ul.features li,td .summary ul.features li,th .summary 
-        ul.features li{display:inline}.behave .step_name,td .step_name,th .step_name
-        {float:left}.behave .step_file,td .step_file,th .step_file{text-align:right;
-        color:#999}.behave .step_file a,td .step_file a,th .step_file a{color:#999}.behave 
-        .scenario_file,td .scenario_file,th .scenario_file{float:right;color:#999}.behave 
-        .tag,td .tag,th .tag{font-weight:700;color:#246ac1}.behave .backtrace,td 
-        .backtrace,th .backtrace{margin-top:0;margin-bottom:0;margin-left:1em;color:#000}
-        .behave a,td a,th a{text-decoration:none;color:#be5c00}.behave a:hover,
-        td a:hover,th a:hover{text-decoration:underline}.behave a:visited,td a:visited,
-        th a:visited{font-weight:400}.behave a div.examples,td a div.examples,
-        th a div.examples{margin:5px 0 5px 15px;color:#000}.behave .outline table,
-        td .outline table,th .outline table{margin:0 0 5px 10px}.behave table,
-        td table,th table{border-collapse:collapse}.behave table td,td table td,
-        th table td{padding:3px 3px 3px 5px}.behave table td.failed,td table td.failed,
-        th table td.failed{border-left:5px solid #c20000;border-bottom:1px solid 
-        #c20000;background:#fffbd3;color:#c20000}.behave table td.passed,td table 
-        td.passed,th table td.passed{border-left:5px solid #65c400;border-bottom:1px 
-        solid #65c400;background:#dbffb4;color:#3d7700}.behave table td.skipped,td 
-        table td.skipped,th table td.skipped{border-left:5px solid #0ff;border-bottom:1px 
-        solid #0ff;background:#e0ffff;color:#011}.behave table td.pending,.behave table 
-        td.undefined,td table td.pending,td table td.undefined,th table td.pending,th table 
-        td.undefined{border-left:5px solid #faf834;border-bottom:1px solid #faf834;
-        background:#fcfb98;color:#131313}.behave table td.message,td table td.message,th 
-        table td.message{border-left:5px solid #0ff;border-bottom:1px solid #0ff;
-        background:#e0ffff;color:#011}.behave ol,td ol,th ol{list-style:none;
-        margin:0;padding:0}.behave ol li.step,td ol li.step,th ol li.step{
-        padding:3px 3px 3px 18px;margin:5px 0 5px 5px}.behave ol li,td ol li,th 
-        ol li{margin:0 0 0 1em;padding:0 0 0 .2em}.behave ol li span.param,td 
-        ol li span.param,th ol li span.param{font-weight:700}.behave ol li.failed,td 
-        ol li.failed,th ol li.failed{border-left:5px solid #c20000;border-bottom:1px 
-        solid #c20000;background:#fffbd3;color:#c20000}.behave ol li.passed,td ol 
-        li.passed,th ol li.passed{border-left:5px solid #65c400;border-bottom:1px 
-        solid #65c400;background:#dbffb4;color:#3d7700}.behave ol li.skipped,td ol 
-        li.skipped,th ol li.skipped{border-left:5px solid #0ff;border-bottom:1px 
-        solid #0ff;background:#e0ffff;color:#011}.behave ol li.pending,.behave ol 
-        li.undefined,td ol li.pending,td ol li.undefined,th ol li.pending,th ol 
-        li.undefined{border-left:5px solid #faf834;border-bottom:1px solid 
-        #faf834;background:#fcfb98;color:#131313}.behave ol li.message,td ol 
-        li.message,th ol li.message{border-left:5px solid #0ff;border-bottom:1px 
-        solid #0ff;background:#e0ffff;color:#011;margin-left:10px}.behave #summary,td 
-        #summary,th #summary{margin:0;padding:5px 10px;text-align:right;top:0;
-        right:0;float:right}.behave #summary p,td #summary p,th #summary 
-        p{margin:0 0 0 2px}.behave #summary #totals,td #summary #totals,th 
-        #summary #totals{font-size:1.2em}"""
+        style = ET.SubElement(head, 'style', type=u"text/css")
+        style.append(ET.Comment(Page.theme.stylesheet_text))
+        script = ET.SubElement(head, 'script', type=u"text/javascript")
+        script_text = ET.Comment(JavascriptLibrary.collapsible)
+        script.append(script_text)
 
         self.stream = self.open()
         body = ET.SubElement(self.html, 'body')
@@ -92,7 +204,7 @@ class HTMLFormatter(Formatter):
         #Summary
         self.header = ET.SubElement(self.suite, 'div', id='behave-header')
         label = ET.SubElement(self.header, 'div', id='label')
-        ET.SubElement(label, 'h1').text = u'Behave features'
+        ET.SubElement(label, 'h1').text = self.title
 
         summary = ET.SubElement(self.header, 'div', id='summary')
 
@@ -103,28 +215,17 @@ class HTMLFormatter(Formatter):
         self.step_totals = ET.SubElement(totals, 'p', id='step_totals')
         self.duration = ET.SubElement(summary, 'p', id='duration')
 
+        # -- PART: Expand/Collapse All
         expand_collapse = ET.SubElement(summary, 'div', id='expand-collapse')
-
-        expander = ET.SubElement(expand_collapse, 'span', id='expander')
-        expander.set('onclick',
-                     "var ols=document.getElementsByClassName('scenario_steps');" +
-                     "for (var i=0; i< ols.length; i++) {" +
-                     "ols[i].style.display = 'block';" +
-                     "}; " +
-                     "return false")
+        expander = ET.SubElement(expand_collapse, 'a', id='expander', href="#")
+        expander.set('onclick', "Collapsible_expandAll('scenario_steps')")
         expander.text = u'Expand All'
-
-        spacer = ET.SubElement(expand_collapse, 'span')
-        spacer.text = u"  "
-
-        collapser = ET.SubElement(expand_collapse, 'span', id='collapser')
-        collapser.set('onclick',
-                      "var ols=document.getElementsByClassName('scenario_steps');" +
-                      "for (var i=0; i< ols.length; i++) {" +
-                      "ols[i].style.display = 'none';" +
-                      "}; " +
-                      "return false")
+        cea_spacer = ET.SubElement(expand_collapse, 'span')
+        cea_spacer.text = u" | "
+        collapser = ET.SubElement(expand_collapse, 'a', id='collapser', href="#")
+        collapser.set('onclick', "Collapsible_collapseAll('scenario_steps')")
         collapser.text = u'Collapse All'
+
 
         self.embed_id = 0
         self.embed_in_this_step = None
@@ -182,10 +283,7 @@ class HTMLFormatter(Formatter):
                                     'id': 'scenario_%s' % self.scenario_id})
 
         self.scenario_name.set('onclick',
-                               "ol=document.getElementById('scenario_%s');" % self.scenario_id +
-                               "ol.style.display =" +
-                               "(ol.style.display == 'none' ? 'block' : 'none');" +
-                               "return false")
+                "Collapsible_toggle('scenario_%s')" % self.scenario_id)
         self.scenario_id += 1
 
     def scenario_outline(self, outline):
@@ -251,10 +349,7 @@ class HTMLFormatter(Formatter):
             self.embed_id += 1
             link = ET.SubElement(step, 'a', {'class': 'message'})
             link.set("onclick",
-                     "rslt=document.getElementById('embed_%s');" % self.embed_id +
-                     "rslt.style.display =" +
-                     "(rslt.style.display == 'none' ? 'block' : 'none');" +
-                     "return false")
+                    "Collapsible_toggle('embed_%s')" % self.embed_id)
             link.text = u'Error message'
 
             embed = ET.SubElement(step, 'pre',
@@ -287,11 +382,7 @@ class HTMLFormatter(Formatter):
         self.embed_id += 1
 
         link = ET.SubElement(span, 'a')
-        link.set("onclick",
-                 "embd=document.getElementById('embed_%s');" % self.embed_id +
-                 "embd.style.display =" +
-                 "(embd.style.display == 'none' ? 'block' : 'none');" +
-                 "return false")
+        link.set("onclick", "Collapsible_toggle('embed_%s')" % self.embed_id)
 
         if 'video/' in mime_type:
             if not caption:
@@ -385,4 +476,5 @@ class HTMLFormatter(Formatter):
 
         # Sending the report to stream
         if len(self.all_features) > 0:
-            self.stream.write(ET.tostring(self.html, pretty_print = True))
+            self.stream.write(u"<!DOCTYPE HTML>\n")
+            self.stream.write(ET_tostring(self.html, pretty_print=True))
