df1dc8d93ab222f1eda9b96adda9a9620c7e4936
jenisys <jenisys@users.noreply.github.com>
2014-06-23 22:36:49 +0200

    Add documented tutorial related to issue #233 problems.

---
+++ more.features/steps/tutorial_steps.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Step implementations for tutorial example."""
+
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
