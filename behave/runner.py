c3bd0d65b53810e0e3e9618f2790b932670d98a8
zgoldstein <zgoldstein@hugeinc.com>
2015-09-14 11:29:25 -0400

    Reverted status display to err buffer.

--- behave/runner.py
+++ behave/runner.py
@@ -716,8 +716,8 @@ class Runner(object):
             current_job.run(self)
             end_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
-            #sys.stderr.write(current_job.status[0]+" ")
-            logging.info(current_job.status[0]+"\n")
+            sys.stderr.write(current_job.status[0]+" ")
+            #logging.info(current_job.status[0]+"\n")
 
             if current_job.type == 'feature':
                 for reporter in self.config.reporters:
