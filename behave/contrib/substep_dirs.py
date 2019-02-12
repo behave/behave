1f4d8b5f5f35b189aab5ff9e10d3ac2386124443
jenisys <jenisys@users.noreply.github.com>
2019-02-12 07:36:24 +0100

    FIX TYPO: Uncovered by NaveenKVN

--- behave/contrib/substep_dirs.py
+++ behave/contrib/substep_dirs.py
@@ -7,10 +7,10 @@ in the "features/steps" directory.
 
     # -- FILE: features/steps/use_substep_dirs.py
     # REQUIRES: path.py
-    from behave.runner_util import load_step_modules 
+    from behave.runner_util import load_step_modules
     from path import Path
-    
-    HERE = Path(__file__).dirname
+
+    HERE = Path(__file__).dirname()
     SUBSTEP_DIRS = list(HERE.walkdirs())
     load_step_modules(SUBSTEP_DIRS)
 """
