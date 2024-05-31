8ba4c7d56788f6117741aeae332798b7b96d1b7b
jenisys <jenisys@users.noreply.github.com>
2024-05-31 23:53:35 +0200

    parse-expression: Provide some basic parse-functions/type-converters

---
+++ behave/parameter_type.py
@@ -0,0 +1,166 @@
+"""
+Provide some "parameter-types" (type-converters) for step parameters
+that can be used in ``parse-expressions`` (step_matcher: "parse"/"cfparse").
+
+EXAMPLE 1::
+
+    # -- FILE: features/steps/example_steps1.py
+    from behave import given, register_type
+    from behave.parameter_type import parse_number
+
+    register_type(Number=parse_number)
+
+    # -- EXAMPLE: "Given I buy 2 apples"
+    @given('I buy {amount:Number} apples'):
+    def step_given_buy_apples(ctx, amount: int):
+        pass
+
+EXAMPLE 2::
+
+    # -- FILE: features/steps/example_steps2.py
+    from behave import given, register_type
+    from behave.parameter_type import parse_number
+    from parse_type import TypeBuilder
+
+    FRUITS = [
+        "apple",  "banana",  "orange",   # -- SINGULAR
+        "apples", "bananas", "oranges",  # -- PLURAL
+    ]
+    parse_fruits = TypeBuilder.make_choice(FRUITS)
+
+    register_type(Fruit=parse_fruit)
+    register_type(Number=parse_number)
+
+    # -- EXAMPLE: "Given I sell 1 apple", "Given I sell 2 bananas", ...
+    @given('I sell {amount:Number} {fruit:Fruit}'):
+    def step_given_sell_fruits(ctx, amount: int, fruit: str):
+        pass
+"""
+
+from __future__ import absolute_import, print_function
+from collections import namedtuple
+import os
+import parse
+from behave import register_type
+
+
+# -----------------------------------------------------------------------------
+# VALUE OBJECT CLASSES
+# -----------------------------------------------------------------------------
+EnvironmentVar = namedtuple("EnvironmentVar", ["name", "value"])
+
+
+# -----------------------------------------------------------------------------
+# TYPE CONVERTERS
+# -----------------------------------------------------------------------------
+@parse.with_pattern(r"\d+")
+def parse_number(text):
+    """
+    Type converter that matches an integer number and converts to an "int".
+
+    :param text:  Text to use.
+    :return: Converted number (as int).
+    :raises: ValueError, if number conversion fails
+    """
+    return int(text)
+
+
+@parse.with_pattern(r".*")
+def parse_any_text(text):
+    """
+    Type converter that matches ANY text (even: EMPTY_STRING).
+
+    EXAMPLE:
+
+    .. code-block:: python
+
+        # -- FILE: features/steps/example_steps.py
+        from behave import step, register_type
+        from behave.step_parameter import parse_any_text
+
+        register_type(AnyText=parse_any_text)
+
+        @step('a parameter with "{param:AnyText}"')
+        def step_use_parameter(context, param):
+            pass
+
+    .. code-block:: gherkin
+
+        # -- FILE: features/example_any_text.feature
+        ...
+        Given a parameter with ""
+        Given a parameter with "one"
+        Given a parameter with "one two three"
+    """
+    return text
+
+
+@parse.with_pattern(r'[^"]*')
+def parse_unquoted_text(text):
+    """
+    Type converter that matches UNQUOTED text (using: double-quotes).
+
+    EXAMPLE:
+
+    .. code-block:: python
+
+        # -- FILE: features/steps/example_steps.py
+        from behave import step, register_type
+        from behave.step_parameter import parse_unquoted_text
+
+        register_type(Unquoted=parse_unquoted_text)
+
+        @step('some parameter with "{param:Unquoted}"')
+        def step_some_parameter(context, param):
+            pass
+    """
+    return text
+
+
+@parse.with_pattern(r"\$\w+")  # -- ONLY FOR: $WORD
+def parse_environment_var(text, default=None):
+    """
+    Matches the name of a process environment-variable, like "$HOME".
+    The name and value of this environment-variable is returned
+    as value-object.
+
+    If the environment-variable is undefined, its value is None.
+
+    :param:  Text to parse/convert (as string).
+    :returns: EnvironmentVar object with name and value.
+
+    EXAMPLE:
+
+    .. code-block:: gherkin
+
+        # -- FILE: features/example_environment_var.feature
+        ...
+        Given I use "$TOP_DIR" as current directory
+    """
+    assert text.startswith("$")
+    env_name = text[1:]
+    env_value = os.environ.get(env_name, default)
+    return EnvironmentVar(env_name, env_value)
+
+
+# -----------------------------------------------------------------------------
+# TYPE REGISTRY:
+# -----------------------------------------------------------------------------
+TYPE_REGISTRY = dict(
+    AnyText=parse_any_text,
+    Number=parse_number,
+    Unquoted=parse_unquoted_text,
+    EnvironmentVar=parse_environment_var,
+)
+
+
+def register_all_types():
+    register_type(**TYPE_REGISTRY)
+
+
+# -----------------------------------------------------------------------------
+# MODULE INIT:
+# -----------------------------------------------------------------------------
+AUTO_REGISTER_TYPE_CONVERTERS = False
+if AUTO_REGISTER_TYPE_CONVERTERS:
+    register_all_types()
---
+++ tests/unit/test_parameter_type.py
@@ -0,0 +1,195 @@
+"""
+Unit tests for :mod:`behave.parameter_type` module.
+"""
+
+from __future__ import absolute_import, print_function
+from contextlib import contextmanager
+import os
+from behave.parameter_type import  (
+    EnvironmentVar,
+    parse_number,
+    parse_any_text,
+    parse_unquoted_text,
+    parse_environment_var,
+)
+from parse import Parser
+import pytest
+
+
+# -----------------------------------------------------------------------------
+# TEST SUPPORT
+# -----------------------------------------------------------------------------
+@contextmanager
+def os_environ():
+    try:
+        initial_environ = os.environ.copy()
+        yield os.environ
+    finally:
+        # -- RESTORE:
+        os.environ = initial_environ
+
+
+# -----------------------------------------------------------------------------
+# TEST SUITE
+# -----------------------------------------------------------------------------
+class TestParseNumber(object):
+    TYPE_REGISTRY = dict(Number=parse_number)
+    PATTERN = "Number: {number:Number}"
+    TEXT_TEMPLATE = "Number: {}"
+
+    @classmethod
+    def assert_match_with_parse_number_and_converts_to_int(cls, text, expected):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        number = result["number"]
+        assert number == expected
+        assert isinstance(number, int)
+
+    @classmethod
+    def assert_mismatch_with_parse_number(cls, text):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        assert result is None
+
+    @pytest.mark.parametrize("text, expected", [
+        ("0", 0),
+        ("12", 12),
+        ("321", 321),
+    ])
+    def test_parse_number__matches_positive_number_and_zero(self, text, expected):
+        self.assert_match_with_parse_number_and_converts_to_int(text, expected)
+
+    @pytest.mark.parametrize("text", ["-1", "-12"])
+    def test_parse_number__mismatches_negavtive_number(self, text):
+        self.assert_mismatch_with_parse_number(text)
+
+
+class TestParseAnyText(object):
+    TYPE_REGISTRY = dict(AnyText=parse_any_text)
+    PATTERN = 'AnyText: "{some:AnyText}"'
+    TEXT_TEMPLATE = 'AnyText: "{}"'
+
+    @classmethod
+    def assert_match_with_parse_any_and_converts_to_string(cls, text, expected):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        actual_value = result["some"]
+        assert actual_value == expected
+        assert isinstance(actual_value, str)
+
+    @classmethod
+    def assert_mismatch_with_parse_any(cls, text):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        assert result is None
+
+    @pytest.mark.parametrize("text", ["Alice", "B_O_B", "charly-123"])
+    def test_parse_any_text__matches_word(self, text):
+        expected = text
+        self.assert_match_with_parse_any_and_converts_to_string(text, expected)
+
+    @pytest.mark.parametrize("text", ["Alice, Bob", "Alice and Bob"])
+    def test_parse_any_text__matches_many_words(self, text):
+        expected = text
+        self.assert_match_with_parse_any_and_converts_to_string(text, expected)
+
+    def test_parse_any_text__matches_empty_string(self):
+        text = ""
+        expected = text
+        self.assert_match_with_parse_any_and_converts_to_string(text, expected)
+
+    @pytest.mark.parametrize("text", [" ", "  ", "\t", "\n"])
+    def test_parse_any_text__matches_whitespace(self, text):
+        expected = text
+        self.assert_match_with_parse_any_and_converts_to_string(text, expected)
+
+
+class TestParseUnquotedText(object):
+    TYPE_REGISTRY = dict(Unquoted=parse_unquoted_text)
+    PATTERN = 'Unquoted: "{some:Unquoted}"'
+    TEXT_TEMPLATE = 'Unquoted: "{}"'
+
+    @classmethod
+    def assert_match_with_parse_unquoted_and_converts_to_string(cls, text, expected):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        actual_value = result["some"]
+        assert actual_value == expected
+        assert isinstance(actual_value, str)
+
+    @classmethod
+    def assert_mismatch_with_parse_unquoted(cls, text):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        assert result is None
+
+    @pytest.mark.parametrize("text", ["Alice", "B_O_B", "charly-123"])
+    def test_parse_unquoted_text__matches_word(self, text):
+        expected = text
+        self.assert_match_with_parse_unquoted_and_converts_to_string(text, expected)
+
+    @pytest.mark.parametrize("text", ["Alice, Bob", "Alice and Bob"])
+    def test_parse_unquoted_text__matches_many_words(self, text):
+        expected = text
+        self.assert_match_with_parse_unquoted_and_converts_to_string(text, expected)
+
+    def test_parse_unquoted_text__matches_empty_string(self):
+        text = ""
+        expected = text
+        self.assert_match_with_parse_unquoted_and_converts_to_string(text, expected)
+
+    @pytest.mark.parametrize("text", [" ", "  ", "\t", "\n"])
+    def test_parse_unquoted_text__matches_whitespace(self, text):
+        expected = text
+        self.assert_match_with_parse_unquoted_and_converts_to_string(text, expected)
+
+    @pytest.mark.parametrize("text", ['Some "more', 'Alice "Bob and Charly"'])
+    def test_parse_unquoted_text__mismatches_string_with_double_quotes(self, text):
+        self.assert_mismatch_with_parse_unquoted(text)
+
+
+class TestParseEnvironmentVar(object):
+    TYPE_REGISTRY = dict(EnvironmentVar=parse_environment_var)
+    PATTERN = 'EnvironmentVar: "{param:EnvironmentVar}"'
+    TEXT_TEMPLATE = 'EnvironmentVar: "{}"'
+
+    @classmethod
+    def assert_match_with_parse_environment_var_returns_to_namedtuple(cls, text, expected):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        actual_param = result["param"]
+        assert actual_param.value == expected
+        assert isinstance(actual_param.value, str)
+        assert isinstance(actual_param, EnvironmentVar)
+
+    @classmethod
+    def assert_match_with_parse_environment_var_and_undefined_returns_namedtuple_with_none(cls, text):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        actual_param = result["param"]
+        assert actual_param.value is None
+        assert isinstance(actual_param, EnvironmentVar)
+
+    @classmethod
+    def assert_mismatch_with_parse_environment_var(cls, text):
+        parser = Parser(cls.PATTERN, extra_types=cls.TYPE_REGISTRY)
+        result = parser.parse(cls.TEXT_TEMPLATE.format(text))
+        assert result is None
+
+    @pytest.mark.parametrize("env_var", [
+        EnvironmentVar("SSH_AGENT", "localhost:12345"),
+        EnvironmentVar("SSH_PID", "1234"),
+    ])
+    def test_parse_environment_var__uses_defined_variable(self, env_var):
+        text = "${}".format(env_var.name)
+        expected = env_var.value
+        with os_environ() as environ:
+            environ[env_var.name] = env_var.value
+            self.assert_match_with_parse_environment_var_returns_to_namedtuple(text, expected)
+
+    def test_parse_environment_var__uses_undefined_variable(self):
+        env_var = EnvironmentVar("UNDEFINED_VAR", None)
+        text = "${}".format(env_var.name)
+        with os_environ() as environ:
+            assert env_var.name not in environ
+            self.assert_match_with_parse_environment_var_and_undefined_returns_namedtuple_with_none(text)
