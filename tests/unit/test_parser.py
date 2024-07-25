behave/behave

commit 2b6845e54131c8587461c80eaa5b3ac26ca46e7f

FIX: Test regression on Windows
Gherkin parser:

* Strip trailing whitespace in multi-line text
  REASON: Whitespace normalization (may contain carriage-return on Windows)

behave/parser.py
@@ -229,7 +229,7 @@ class Parser(object):
 
         for line in text.split("\n"):
             self.line += 1
-            if not line.strip() and self.state != "multiline":
+            if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.
                 continue
             self.action(line)
@@ -381,7 +381,7 @@ class Parser(object):
         return None
 
     def action(self, line):
-        if line.strip().startswith("#") and self.state != "multiline":
+        if line.strip().startswith("#") and self.state != "multiline_text":
             if self.state != "init" or self.tags or self.variant != "feature":
                 return
 
@@ -584,8 +584,24 @@ class Parser(object):
         # pylint: disable=R0911
         #   R0911   Too many return statements (8/6)
         stripped = line.lstrip()
+        # if self.statement.steps:
+        #     # -- ENSURE: Multi-line text follows a step.
+        #     if stripped.startswith('"""') or stripped.startswith("'''"):
+        #         # -- CASE: Multi-line text (docstring) after a step detected.
+        #         self.state = "multiline_text"
+        #         self.multiline_start = self.line
+        #         self.multiline_terminator = stripped[:3]
+        #         self.multiline_leading = line.index(stripped[0])
+        #         return True
+
         if stripped.startswith('"""') or stripped.startswith("'''"):
-            self.state = "multiline"
+            # -- CASE: Multi-line text (docstring) after a step detected.
+            # REQUIRE: Multi-line text follows a step.
+            if not self.statement.steps:
+                raise ParserError("Multi-line text before any step",
+                                  self.line, self.filename)
+
+            self.state = "multiline_text"
             self.multiline_start = self.line
             self.multiline_terminator = stripped[:3]
             self.multiline_leading = line.index(stripped[0])
@@ -602,25 +618,47 @@ class Parser(object):
             return True
 
         if line.startswith("|"):
-            assert self.statement.steps, "TABLE-START without step detected."
+            # -- CASE: TABLE-START detected for data-table of a step
+            # OLD: assert self.statement.steps, "TABLE-START without step detected"
+            if not self.statement.steps:
+                raise ParserError("TABLE-START without step detected",
+                                  self.line, self.filename)
             self.state = "table"
             return self.action_table(line)
 
         return False
 
-    def action_multiline(self, line):
+    def action_multiline_text(self, line):
+        """Parse remaining multi-line/docstring text below a step
+        after the triple-quotes were detected:
+
+        * triple-double-quotes or
+        * triple-single-quotes
+
+        Leading and trailing triple-quotes must be the same.
+
+        :param line:  Parsed line, as part of a multi-line text (as string).
+        """
         if line.strip().startswith(self.multiline_terminator):
-            step = self.statement.steps[-1]
-            step.text = model.Text(u"\n".join(self.lines), u"text/plain",
-                                   self.multiline_start)
-            if step.name.endswith(":"):
-                step.name = step.name[:-1]
+            # -- CASE: Handle the end of a multi-line text part.
+            # Store the multi-line text in the step object (and continue).
+            this_step = self.statement.steps[-1]
+            text = u"\n".join(self.lines)
+            this_step.text = model.Text(text, u"text/plain", self.multiline_start)
+            if this_step.name.endswith(":"):
+                this_step.name = this_step.name[:-1]
+
+            # -- RESET INTERNALS: For next step
             self.lines = []
             self.multiline_terminator = None
-            self.state = "steps"
+            self.state = "steps"    # NEXT-STATE: Accept additional step(s).
             return True
 
-        self.lines.append(line[self.multiline_leading:])
+        # -- SPECIAL CASE: Strip trailing whitespace (whitespace normalization).
+        # HINT: Required for Windows line-endings, like "\r\n", etc.
+        text_line = line[self.multiline_leading:].rstrip()
+        self.lines.append(text_line)
+
         # -- BETTER DIAGNOSTICS: May remove non-whitespace in execute_steps()
         removed_line_prefix = line[:self.multiline_leading]
         if removed_line_prefix.strip():
@@ -631,35 +669,46 @@ class Parser(object):
         return True
 
     def action_table(self, line):
-        line = line.strip()
+        """Parse a table, with pipe-separated columns:
 
+        * Data table of a step (after the step line)
+        * Examples table of a ScenarioOutline
+        """
+        line = line.strip()
         if not line.startswith("|"):
+            # -- CASE: End-of-table detected
             if self.examples:
+                # -- CASE: Examples table of a ScenarioOutline
                 self.examples.table = self.table
                 self.examples = None
             else:
+                # -- CASE: Data table of a step
                 step = self.statement.steps[-1]
                 step.table = self.table
                 if step.name.endswith(":"):
                     step.name = step.name[:-1]
+
+            # -- RESET: Parameters for parsing the next step(s).
             self.table = None
             self.state = "steps"
             return self.action_steps(line)
 
         if not re.match(r"^(|.+)\|$", line):
             logger = logging.getLogger("behave")
-            logger.warning(u"Malformed table row at %s: line %i", self.feature.filename, self.line)
+            logger.warning(u"Malformed table row at %s: line %i",
+                           self.feature.filename, self.line)
 
         # -- SUPPORT: Escaped-pipe(s) in Gherkin cell values.
         #    Search for pipe(s) that are not preceeded with an escape char.
         cells = [cell.replace("\\|", "|").strip()
                  for cell in re.split(r"(?<!\\)\|", line[1:-1])]
         if self.table is None:
+            # -- CASE: First row of the table
             self.table = model.Table(cells, self.line)
         else:
+            # -- CASE: Following rows of the table
             if len(cells) != len(self.table.headings):
                 raise ParserError(u"Malformed table", self.line, self.filename)
-                # MAYBE: self.filename)
             self.table.add_row(cells, self.line)
         return True
 
@@ -689,7 +738,7 @@ class Parser(object):
 
         for line in text.split("\n"):
             self.line += 1
-            if not line.strip() and self.state != "multiline":
+            if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.
                 continue
             self.action(line)
@@ -784,7 +833,7 @@ class Parser(object):
 
         for line in text.split("\n"):
             self.line += 1
-            if not line.strip() and self.state != "multiline":
+            if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.
                 continue
             self.action(line)

tests/unit/test_parser.py
@@ -1740,7 +1740,7 @@ Then the last step has a final table:
             ("then",  "Then",  "the last step has a final table", None, table2),
         ])
 
-    def test_parse_steps_with_malformed_table(self):
+    def test_parse_steps_with_malformed_table_fails(self):
         text = u'''
 Given a step with a malformed table:
     | Name   | City |
@@ -1749,3 +1749,27 @@ Given a step with a malformed table:
 '''.lstrip()
         with pytest.raises(parser.ParserError):
             parser.parse_steps(text)
+
+    def test_parse_steps_with_multiline_text_before_any_step_fails(self):
+        text = u'''
+  """
+  BAD MULTI-LINE TEXT (before any step)
+  """
+Given another step
+'''.lstrip()
+        with pytest.raises(parser.ParserError) as exc:
+            parser.parse_steps(text)
+
+        assert exc.match("Multi-line text before any step")
+
+    def test_parse_steps_with_datatable_before_any_step_fails(self):
+        text = u'''
+          | name  | birthyear |
+          | Alice | 1980 |
+          | Bob   | 2005 |
+        Given another step
+        '''.lstrip()
+        with pytest.raises(parser.ParserError) as exc:
+            parser.parse_steps(text)
+
+        assert exc.match("TABLE-START without step detected")

commit 5e1756407ec6093eef6f676f7b867cb99855aeb9

FIX #1189: Do not strip trailing whitespace from multiline text
Gherkin parser:

Commit 2b6845e54131 ("FIX: Test regression on Windows") introduced
whitespace normalization when parsing feature files. The motivation was
to support files with Windows line endings (CRLF). The normalization is
achieved by splitting lines on Unix line endings (LF) and removing
trailing any whitespace in multi-line text statements.

It turns out that this normalization is a bit too aggressive: it remove
any trailing whitespace -- not only the extra CR character -- from lines
within the multiline text statement.

Address the issue by changing the line handling: instead of splitting on
Unix LF and cleanup leftovers inside parser actions, split the lines
correctly using the built-in str.splitlines() which supports a superset
of universal newlines. This has the advantage that we get consistent
behavior for all kinds of line separators -- including Windows, Unix,
classic Mac.

Fixes: 2b6845e54131 ("FIX: Test regression on Windows")
Signed-off-by: Lucas Kahlert <lucas.kahlert@square-src.de>

behave/parser.py
@@ -237,7 +237,7 @@ class Parser(object):
         self.reset()
         self.filename = filename
 
-        for line in text.split("\n"):
+        for line in text.splitlines():
             self.line += 1
             if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.
@@ -666,9 +666,7 @@ class Parser(object):
             self.state = "steps"    # NEXT-STATE: Accept additional step(s).
             return True
 
-        # -- SPECIAL CASE: Strip trailing whitespace (whitespace normalization).
-        # HINT: Required for Windows line-endings, like "\r\n", etc.
-        text_line = line[self.multiline_leading:].rstrip()
+        text_line = line[self.multiline_leading:]
         self.lines.append(text_line)
 
         # -- BETTER DIAGNOSTICS: May remove non-whitespace in execute_steps()
@@ -748,7 +746,7 @@ class Parser(object):
         self.statement = self.rule
         self.state = "rule"
 
-        for line in text.split("\n"):
+        for line in text.splitlines():
             self.line += 1
             if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.
@@ -866,7 +864,7 @@ class Parser(object):
         self.statement = model.Scenario(filename, 0, u"scenario", u"")
         self.state = "steps"
 
-        for line in text.split("\n"):
+        for line in text.splitlines():
             self.line += 1
             if not line.strip() and self.state != "multiline_text":
                 # -- SKIP EMPTY LINES, except in multiline string args.

tests/unit/test_parser.py
@@ -984,6 +984,38 @@ Feature: Stuff
         with pytest.raises(ParserError):
             parse_feature(text)
 
+    def test_parses_multiline_text_and_preserves_trailing_whitespace(self):
+        # -- ISSUE 1189: Parser removes trailing whitespace in multiline text string.
+        doc = u'''
+Feature: Multiline
+
+  Scenario: Multiline Text with Trailing Whitespace
+    Given a multiline argument with:
+      """
+      Trailing    
+      whitespace at   
+      the end of each   
+      line      
+      """
+    Then the trailing whitespace in each line ist not stripped
+'''.lstrip()
+        feature = parse_feature(doc)
+        assert feature is not None
+        assert feature.name == "Multiline"
+        assert len(feature.scenarios) == 1
+        assert feature.scenarios[0].name == "Multiline Text with Trailing Whitespace"
+        text = "\n".join([
+            "Trailing    ",
+            "whitespace at   ",
+            "the end of each   ",
+            "line      ",
+        ])
+        # pylint: disable=bad-whitespace
+        assert_compare_steps(feature.scenarios[0].steps, [
+            ('given', 'Given', 'a multiline argument with:', text, None),
+            ('then', 'Then', 'the trailing whitespace in each line ist not stripped', None, None),
+        ])
+
 
 class TestParser4AndButSteps(object):
     def test_parse_scenario_with_and_and_but(self):