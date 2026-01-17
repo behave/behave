15d19154c556719cd608f33f5b3ee70f82eb3951
SKAUL05 <kaul.sarath@gmail.com>
2026-01-17 07:44:06 -0600

    Updated Docstring

--- behave/model.py
+++ behave/model.py
@@ -884,6 +884,16 @@ class Scenario(TagAndStatusStatement, Replayable):
        glorified strings) attached to the scenario.
        See :ref:`controlling things with tags`.
 
+    .. attribute:: effective_tags
+
+        A set of tags that apply to this scenario, including those inherited
+        from parent entities (Feature, Rule, etc.).
+
+        This is different from ``tags``, which only lists the tags explicitly
+        declared on the scenario itself.
+
+        .. versionadded:: 1.2.7
+
     .. attribute:: status
 
        Read-Only. A summary status of the scenario's run. If read before the
@@ -929,15 +939,6 @@ class Scenario(TagAndStatusStatement, Replayable):
 
         .. versionadded:: 1.2.7
 
-    .. attribute:: effective_tags
-
-        A list of tags that apply to this scenario, including those inherited from
-        the Feature.
-
-        This is different from ``tags``, which only lists the tags explicitly
-        declared on the scenario itself.
-
-        .. versionadded:: 1.2.7
 
     .. _`scenario`: gherkin.html#scenarios
     """
